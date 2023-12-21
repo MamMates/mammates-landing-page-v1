40b75462b84e5d6c5fc09d82260916da7a184145
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2023-12-22 00:02:04 +0800

    adding some meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Navbar from "@/components/widgets/navbar/Navbar";
 import Footer from "@/components/widgets/footer/Footer";
 import HomeSellerMockup from "@/components/views/home/HomeSellerMockup";
 import HomeSeller from "@/components/views/home/HomeSeller";
 import HomeBuyerMockup from "@/components/views/home/HomeBuyerMockup";
 import HomeBuyer from "@/components/views/home/HomeBuyer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Box>
+      <Head>
+        <title>Saving food, Sharing hope</title>
+        <meta
+          name="description"
+          content="Welcome to MamMates – your gateway to a vibrant culinary universe. Explore unique dishes, connect with passionate sellers, and savor a personalized food journey. Whether you're a discerning buyer or an aspiring seller, MamMates is where culinary dreams come to life. Join us in the joy of saving food and sharing hope. Experience more than just a marketplace; experience a community united by the love of good food."
+        />
+      </Head>
       <Navbar />
       <HomeSellerMockup />
       <HomeSeller />
